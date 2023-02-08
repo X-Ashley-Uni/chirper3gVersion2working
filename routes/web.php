75913dc225eca28735d235853420e561f887c8e6
CIT183G-17@M303.UC-BCF.EDU.PH <CIT183G-17@M303.UC-BCF.EDU.PH>
2023-02-08 17:37:40 +0800

    new branch1

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login'); // this shows the login page immediately, original is 'welcome'
 });
 
 Route::get('/dashboard', function () {
