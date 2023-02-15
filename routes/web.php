265d26e509159ee08cc4b1a051f4b73bca24becd
CIT183G-17@M303.UC-BCF.EDU.PH <CIT183G-17@M303.UC-BCF.EDU.PH>
2023-02-15 16:44:04 +0800

    These files has changes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/users',[UserController::class, 'index']);
+Route::get( '/user/{id}',[UserController::class, 'show']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
